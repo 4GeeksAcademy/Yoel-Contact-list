824b93660030c9e1e9fe73cc0eb6456f834c4713
Yoel Cabaleiro <yoelcabaleiro@gmail.com>
2023-11-18 22:28:13 +0000

    added actions

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -26,13 +26,97 @@ const getState = ({ getStore, getActions, setStore }) => {
 				};
 				const response = await fetch(url, options)
 				if(response.ok){
-					alert(`${object.full_name} added to ${object.agenda_slug}!`)
+					alert(`${object.full_name} added to "${object.agenda_slug}!"`)
 				}
 				else{
 					alert("Sorry, somenthing went wrong.")
 					console.log("Error :", response.status, response.statusText)
 				}
-			}
+			},
+
+			updateContact: async(object) => {
+				const url = "https://playground.4geeks.com/apis/fake/contact/";
+				const options = {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(object)            
+				};
+				const response = await fetch(url, options)
+				if(response.ok){
+					alert(`${object.full_name} updated in "${object.agenda_slug}!"`)
+				}
+				else{
+					alert("Sorry, somenthing went wrong.")
+					console.log("Error :", response.status, response.statusText)
+				}
+			},
+
+			deleteContact: async(object) => {
+				const url = `https://playground.4geeks.com/apis/fake/contact/${object.id}`;
+				const options = {
+					method: "DELETE"          
+				};
+				const response = await fetch(url, options)
+				if(response.ok){
+					alert(`${object.full_name} deleted from "${object.agenda_slug}"!`)
+				}
+				else{
+					alert("Sorry, somenthing went wrong.")
+					console.log("Error :", response.status, response.statusText)
+				}
+			},
+
+			deleteAgenda: async(agenda) => {
+				const url = `https://playground.4geeks.com/apis/fake/contact/agenda/${agenda}`;
+				const options = {
+					method: "DELETE"           
+				};
+				const response = await fetch(url, options)
+				if(response.ok){
+					alert(`Agenda "${agenda}" was deleted.`)
+				}
+				else{
+					alert("Sorry, somenthing went wrong.")
+					console.log("Error :", response.status, response.statusText)
+				}
+
+			},
+
+			getAgenda: async(agenda) => {
+				const url = `https://playground.4geeks.com/apis/fake/contact/agenda/${agenda}`;
+				const options = {
+					method: "GET"           
+				};
+				const response = await fetch(url, options)
+				if(response.ok){
+					const data = response.json()
+					return data
+				}
+				else{
+					alert("Sorry, somenthing went wrong.")
+					console.log("Error :", response.status, response.statusText)
+				}
+
+			},
+
+			getContact: async(id) => {
+				const url = `https://playground.4geeks.com/apis/fake/contact/${id}`;
+				const options = {
+					method: "GET"           
+				};
+				const response = await fetch(url, options)
+				if(response.ok){
+					const data = response.json()
+					return data
+				}
+				else{
+					alert("Sorry, somenthing went wrong.")
+					console.log("Error :", response.status, response.statusText)
+				}
+
+			}					
 		}
 	};
 };
